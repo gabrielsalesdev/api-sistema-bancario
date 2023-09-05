7eb1441ac5ba57abcf60468b7a9dc165686361b7
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-09-04 23:38:32 -0300

    chore: excluindo todos os arquivos

--- src/middlewares/error.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.statusCode || 500).json({ mensagem: err.message } || { mensagem: 'Erro interno do servidor' });
-};
-
-module.exports = { errorHandler };
