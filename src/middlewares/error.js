11701e0a6ec63723b8ef4714bc9160995a3c93aa
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-10-01 23:02:41 -0300

    refactor: alterando a mensagem de erro

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack);
-    res.status(err.statusCode || 500).json({ mensagem: err.message } || { mensagem: 'Erro interno do servidor' });
+    res.status(err.statusCode || 500).json({ mensagem: err.message || 'Erro interno do servidor' });
 };
 
 module.exports = { errorHandler };
