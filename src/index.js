49d56698fce6a1bbb347067f184f97c5869d11bf
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-10-18 10:33:56 -0300

    feat: implementando o intermediário de cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const errorMiddlewares = require('./middlewares/error');
-const routes = require('./routes');
 const app = express();
 const port = 3000;
+
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const errorMiddlewares = require('./middlewares/error');
+
+app.use(cors());
 
 app.use(express.json());
 
